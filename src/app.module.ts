9c782ba5ad04c3a42cad3778ff0f5f54b19790b0
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-05-29 17:57:09 +0200

    integrate graphql with nest app

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppService } from './app.service';
+import { join } from 'path';
+import { CatsModule } from './cats/cats.module';
+
+@Module({
+  imports: [
+    CatsModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
