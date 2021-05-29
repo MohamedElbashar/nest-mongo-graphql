fc2bafa8fe7a9160b16730cb2a66f809bbac9600
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-05-29 19:31:27 +0200

    add cat module

--- src/cats/cats.resolver.ts
+++ src/cats/cats.resolver.ts
@@ -1,14 +1,22 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Query, Resolver, Mutation, Args } from '@nestjs/graphql';
+import { CatsService } from './cats.service';
+import { CatType } from './dto/create-cat.dto';
+import { CatInput } from './inputs/cat.input';
 
 @Resolver()
 export class CatsResolver {
-  //   constructor(
-  //     private authorsService: AuthorsService,
-  //     private postsService: PostsService,
-  //   ) {}
+  constructor(private catsService: CatsService) {}
 
   @Query(() => String)
   async hello() {
     return 'hello world';
+  }
+  @Query(() => [CatType])
+  async cats() {
+    return this.catsService.findAll();
+  }
+  @Mutation(() => CatType)
+  async createCat(@Args('input') input: CatInput) {
+    return this.catsService.create(input);
   }
 }
